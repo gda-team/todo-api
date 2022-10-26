1626938bb1dd16bf5729feed810d88d397819188
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-26 12:31:36 +0200

    feat: take a random id

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,3 +3,7 @@
 exports.getTodo = (req, res, next) => {
 	res.json({ message: 'hello ' });
 };
+exports.postAddTodo = (req, res, next) => {
+	this.id = Math.random().toString();
+	res.json({ message: 'hello ' });
+};
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,5 +3,6 @@ const { getTodo } = require('../controllers/todo.controller');
 const router = require('express').Router();
 
 router.get('/', getTodo);
+router.get('/add-todo', postAddTodo);
 
 module.exports = router;
